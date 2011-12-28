7eacdb298ed6f8323fd0b420b121416e015fd66a
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-28 17:56:33 +0900

    Change to use $srcdir to install from another directory.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+srcdir=$(dirname $0)
 scmdir=$(pkg-config --variable=uim_scmdir uim)
 pixmapsdir=$(pkg-config --variable=uim_datadir uim)/pixmaps
-cp external-editor.scm external-editor-custom.scm "$scmdir"
-cp pixmaps/external-editor.png pixmaps/external-editor_dark_background.png "$pixmapsdir"
-cp pixmaps/external-editor.svg pixmaps/external-editor_dark_background.svg "$pixmapsdir"
+cp "$srcdir/external-editor.scm" "$srcdir/external-editor-custom.scm" "$scmdir"
+cp "$srcdir/pixmaps/external-editor.png" "$srcdir/pixmaps/external-editor_dark_background.png" "$pixmapsdir"
+cp "$srcdir/pixmaps/external-editor.svg" "$srcdir/pixmaps/external-editor_dark_background.svg" "$pixmapsdir"
 uim-module-manager --register external-editor
